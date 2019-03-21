d7a0122441c6930dca3e2e50ea1328bf82105ab4
Breno Calazans <brenocalazans@gmail.com>
2019-03-20 21:26:33 -0300

    Fix slug and url for category query

--- node/resolvers/catalog/category.ts
+++ node/resolvers/catalog/category.ts
@@ -1,19 +1,55 @@
-import { compose, last, prop, split } from 'ramda'
+import { compose, last, prop, split, find } from 'ramda'
 import { toIOMessage } from '../../utils/ioMessage'
-
 const lastSegment = compose<string, string[], string>(last, split('/'))
+
+interface Category {
+  id: string,
+  url: string,
+  children: any[],
+}
 
 export const resolvers = {
   Category: {
     cacheId: prop('id'),
 
-    href: prop('url'),
+    href: async ({ id }, _, { dataSources: { catalog } }) => {
+      const categories = await catalog.categories(3) as Category[]
+
+      const flattenCategories = categories.reduce(
+        (acc : Category[], category) => acc.concat(category, category.children),
+        []
+      )
+
+      const category = find(
+        (c : Category) => c.id === id,
+        flattenCategories
+      ) || { url: '' }
+
+      return category.url.replace(
+        /https:\/\/[A-z0-9]+\.vtexcommercestable\.com\.br/,
+        ''
+      )
+    },
 
     metaTagDescription: prop('MetaTagDescription'),
 
     name: ({name}, _, ctx) => toIOMessage(ctx, name),
 
-    slug: ({url}) => url ? lastSegment(url) : null,
+    slug: async ({ id }, _, { dataSources: { catalog } }) => {
+      const categories = await catalog.categories(3) as Category[]
+
+      const flattenCategories = categories.reduce(
+        (acc : Category[], category) => acc.concat(category, category.children),
+        []
+      )
+
+      const category = find(
+        (c : Category) => c.id === id,
+        flattenCategories
+      ) || { url: '' }
+
+      return category.url ? lastSegment(category.url) : null
+    },
 
     titleTag: prop('Title'),
   }
